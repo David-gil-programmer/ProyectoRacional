6466ea7f2a42ba3083158d1b97dc9bc620f4c2f5
David Ramos <ramos_drh@hotmail.com>
2019-12-11 17:53:54 +0100

    Sumo, divido y multiplico z y k y lo muestro

--- src/main/java/com/mycompany/racionaldaviddavid/AppRacional.java
+++ src/main/java/com/mycompany/racionaldaviddavid/AppRacional.java
@@ -10,25 +10,31 @@ package com.mycompany.racionaldaviddavid;
  * @author ramos-drh
  */
 public class AppRacional {
+
     public static void main(String[] args) {
         Racional z = new Racional(4, 5);
         Racional k = new Racional(3, 4);
-        
+
         Racional racAleatorio;
         racAleatorio = Racional.aleatorio();
 
         // mostrar los tres resultados
-        
         z.imprimirConsola();
         k.imprimirConsola();
         racAleatorio.imprimirConsola();
-        
+
         // comparar z y k y mostrarlo por pantalla
-        if(Racional.igualdad(z, k)){
+        if (Racional.igualdad(z, k)) {
             System.out.println("Son iguales");
-    }else{
-        System.out.println("No son iguales");
+        } else {
+            System.out.println("No son iguales");
         }
-}
-    
+        
+        z.suma(k);
+        System.out.println(z);
+        System.out.println(Racional.division(z,k));
+        z.multiplicar(k);
+        System.out.println(z);
+    }
+
 }
