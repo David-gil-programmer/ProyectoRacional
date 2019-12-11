a53b5a5a1ee78b3da7d75a851c36a3b166a5391f
David <ramos_drh@hotmail.com>
2019-12-11 14:36:27 +0100

    Añado la clase de prueba y 2 instancias de la clase Racional

---
+++ src/main/java/com/mycompany/racionaldaviddavid/AppRacional.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.racionaldaviddavid;
+
+/**
+ *
+ * @author ramos-drh
+ */
+public class AppRacional {
+    public static void main(String[] args) {
+        Racional z = new Racional(4, 5);
+        Racional k = new Racional(3, 4);
+        
+        
+    }
+}
